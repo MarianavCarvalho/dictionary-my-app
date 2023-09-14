ba5204b1f87106c938de6724c303a20d1119caf5
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-09-14 17:59:53 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
-import axios from "axios";
+
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let[keyword, setKeyword] = useState("");
 
 
-    function handleResponse(response){
-        console.log(response.data[0])
-    }
+    
 
     function search(event){
         event.preventDefault();
-    
-       let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=a1b283feoeccefb140t55b69080a1da6`;
-       axios.get(apiUrl).then(handleResponse)
+    alert(`${keyword}`);
+      
     }
     
 
